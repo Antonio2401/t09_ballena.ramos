966696339446abba4eb7774ebb06827f36c4d14e
BALLENA_CUSTODIO <aballenac@unprg.edu.pe>
2019-12-20 10:34:21 -0500

    se agrego las carpetas

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # t09_ballena.ramos
 validadores con funciones
+ballena custodio antonio
+ramos flores juan daniel
---
+++ ballena_custodio/app.py
@@ -0,0 +1,9 @@
+
+import libreria
+import os
+x=int(os.sys.argv[1])
+y=int(os.sys.argv[2])
+
+res=libreria.area_rectangulo(x,y)
+print("el area del rectangulo es:",res)
+
